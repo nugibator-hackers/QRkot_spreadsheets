011e6fbd29e13968ff44f6c5543a494a87d4d7e3
Nikolai <kolya.kurenkov.1999@gmail.com>
2025-02-17 12:53:13 +0300

    F

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -1,10 +1,4 @@
-from aiogoogle import Aiogoogle
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app.core.db import get_async_session
-from app.core.google_client import get_service
-from app.core.user import current_superuser
+from fastapi import APIRouter
 
 
 router = APIRouter()
